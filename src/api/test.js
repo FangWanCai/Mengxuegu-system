7ef045e564ec9bc920c1b963f186203e5c71a3b1
FangWanCai <2412119616@qq.com>
2023-06-30 18:05:46 +0800

    feat:封装api

---
+++ src/api/test.js
@@ -0,0 +1,14 @@
+// 引入request
+import { request } from "../utils/request";
+
+const mebbier = (data)=>{
+    return request({
+        url: '',
+        method: '',
+        data,
+    })
+}
+
+export default {
+    mebbier,
+}
