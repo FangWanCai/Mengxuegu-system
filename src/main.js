d29a1c748b8370b5043698a82aafc6c74f84b3b0
FangWanCai <2412119616@qq.com>
2023-06-30 10:56:06 +0800

    init:在main.js引入axios

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
+//引入axios
+import axios from 'axios';
+//把axios挂载到vue上
+Vue.prototype.$axios = axios;
+
 Vue.config.productionTip = false;
 
 new Vue({
